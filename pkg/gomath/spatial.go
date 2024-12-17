acc95b8d065709d3602e593b1c8d437a20410407
Mike <mikeresnik@gmail.com>
2024-12-17 10:11:58 -0500

    Bump to v1.1.3

--- pkg/gomath/spatial.go
+++ pkg/gomath/spatial.go
@@ -1,6 +1,10 @@
 package gomath
 
-import "github.com/mtresnik/goutils/pkg/goutils"
+import (
+	"github.com/mtresnik/goutils/pkg/goutils"
+	"hash/fnv"
+	"math"
+)
 
 type Spatial interface {
 	GetValues() []float64
@@ -10,6 +14,21 @@ type Spatial interface {
 	Y() float64
 	Z() float64
 	W() float64
+}
+
+func HashSpatial(spatial Spatial) int64 {
+	hasher := fnv.New64a()
+
+	for _, value := range spatial.GetValues() {
+		bits := math.Float64bits(value)
+		buf := make([]byte, 8)
+		for i := 0; i < 8; i++ {
+			buf[i] = byte(bits >> (i * 8))
+		}
+		_, _ = hasher.Write(buf)
+	}
+
+	return int64(hasher.Sum64())
 }
 
 func ToVector(sp Spatial) Vector {
